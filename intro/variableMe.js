b2d439c850b2faa2a10e374071a568c65dcf81c6
Soumalya Karak <soumalyakarak2@gmail.com>
2025-03-30 16:19:41 +0530

    3rd commit

---
+++ intro/variableMe.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Hitesh";
+greetings.toLowerCase();
+console.log(greetings);
+var userId = 334455.3;
+userId.toFixed();
+// userId = "hitesh"
+// boolean
+var isLoggedIn = false;
+// any
+var hero;
+function getHero() {
+    return {};
+}
+hero = getHero();
---
+++ intro/variableMe.ts
@@ -0,0 +1,23 @@
+let greetings: string = "Hello Hitesh";
+
+greetings.toLowerCase()
+console.log(greetings);
+
+let userId = 334455.3
+
+userId.toFixed()
+// userId = "hitesh"
+// boolean
+let isLoggedIn: boolean = false
+
+// any
+
+let hero: object;
+
+function getHero(){
+    return {}
+}
+
+hero = getHero()
+
+export {}
