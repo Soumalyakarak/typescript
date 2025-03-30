7ea52d3c50e10e9fa96597888a44372bc871b081
Soumalya Karak <soumalyakarak2@gmail.com>
2025-03-30 16:46:40 +0530

    4th commit

---
+++ basics/myArray.js
@@ -0,0 +1,11 @@
+var superHeros = [];
+// const HeroPower: object[] = [{}]
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
---
+++ basics/myArray.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = []
+// const HeroPower: object[] = [{}]
+const heroPower: Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name: "", isActive: true})
